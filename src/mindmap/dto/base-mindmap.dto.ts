151e7e52af345cc39abde010a18c5c591eb4cd6a
Dao Xuan Quy <wolflavamc@gmail.com>
2024-10-14 20:33:41 +0700

    fix: swagger demo

--- src/mindmap/dto/base-mindmap.dto.ts
+++ src/mindmap/dto/base-mindmap.dto.ts
@@ -26,13 +26,13 @@ export class BaseMindmapDto {
 
   @ApiProperty({
     default: [
-      '350a90b5-499f-4000-a1e8-8b40964e5a2f',
-      'a67d94fb-9127-4fef-b970-b16320ee2212',
-      '92024fc0-d98f-4278-8ec6-281cf2a9b280',
-      '08bb4f6e-940d-45fc-8803-ce954f695929',
-      'be737b17-2cba-4679-af94-0a62806d66b3',
-      '29e61784-4724-45c0-9f13-85e656e0e11a',
-    ],
+    "9507b06e-2da5-47f3-a25c-7b8a8b5b251b",
+    "8d50f01c-e880-4465-837c-ef86c5957093",
+    "11139a5d-6219-4028-9ca7-c962190b5c6c",
+    "3c5bf991-5e3d-4e49-b1ce-3ca3eb252eb2",
+    "221298c5-2873-43f5-ae9a-68d6282e30f8",
+    "c6f2192f-1cf4-4a49-9025-3e6cf2d8d92e"
+  ]
   })
   @IsArray()
   documentsId: string[];
