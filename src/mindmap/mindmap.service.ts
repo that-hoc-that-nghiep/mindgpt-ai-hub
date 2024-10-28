28541f88fa0c2e293690dfab809cbc8b2417f2f3
LiusDev <quydx.work@gmail.com>
2024-10-28 17:08:36 +0700

    fix: chat prompt

--- src/mindmap/mindmap.service.ts
+++ src/mindmap/mindmap.service.ts
@@ -121,12 +121,11 @@ export class MindmapService {
       [
         'system',
         `You are an expert mindmap designer and your task is answer user's question for summarizing mindmap or provide knowledge from mindmap with following requirements:
-        1. Required answer in markdown format.
-        2. The language used in answer must be the same with the language used in the user's input.
-        3. Answers have a specific layout structure.
-        4. It is allowed to refer to the mermaid diagram to understand the content of the mindmap, however, it is only allowed to answer questions related to the nodes selected by the user.
-        5. If there are documents context, you are only allowed to answer the knowledge contained in the documents and mermaid. Absolutely do not arbitrarily create answers
-        6. Do not include selected nodes in the answer
+        1. The language used in answer must be the same with the language used in the user's input.
+        2. Answers have a specific layout structure.
+        3. It is allowed to refer to the mermaid diagram to understand the content of the mindmap, however, it is only allowed to answer questions related to the nodes selected by the user.
+        4. If there are documents context, you are only allowed to answer the knowledge contained in the documents and mermaid. Absolutely do not arbitrarily create answers
+        5. Do not include selected nodes in the answer
 
         {context}`,
       ],
